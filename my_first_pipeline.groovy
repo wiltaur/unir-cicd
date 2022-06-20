d7605db57943f8ae794930dc21b6de4942479797
wiltaur <36390645+wiltaur@users.noreply.github.com>
2022-06-20 15:50:31 -0500

    Update my_first_pipeline.groovy

--- my_first_pipeline.groovy
+++ my_first_pipeline.groovy
@@ -20,11 +20,32 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
+        stage('Api tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage(E2e tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
     }
     post {
         always {
             junit 'results/*_result.xml'
             cleanWs()
+        }
+        success {
+            echo 'Pipeline finish successful'
+        }
+        failure {
+            /* mail to: 'wssalgado@test.com', 
+            subject: "Error in My First JenkinFile", 
+            body: "Job '${JOB_NAME}' with Build Number (${BUILD_NUMBER}) is failed, at least one step failed. Please go to ${BUILD_URL} and verify the build." */
+            echo 'Job (${JOB_NAME}) with Build Number (${BUILD_NUMBER}) is failed, at least one step failed. Please go to ${BUILD_URL} and verify the build.'
         }
     }
 }
